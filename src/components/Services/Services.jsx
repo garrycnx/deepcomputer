12f16b622f77b2ed6a11934328179214ee8fa8cd
garrycnx <garrycnx@gmail.com>
2026-01-25 16:57:34 +0530

    Initial commit - Deep Computer website

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,9 @@
 import "./Services.css";
 
-import aadhaar from "../../assets/services/Aadhaar.png";
+import aadhaar from "../../assets/services/aadhaar.png";
 import gnm from "../../assets/services/number_plate.png";
 import passport from "../../assets/services/passport.png";
-import driving from "../../assets/services/Driving.png";
+import driving from "../../assets/services/driving.png";
 
 const services = [
   { name: "Aadhaar Card",punjabi: "ਆਧਾਰ ਕਾਰਡ", img: aadhaar },
