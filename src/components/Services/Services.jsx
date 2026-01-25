9bf73c3126f35c76b7896974e5e3b5d2ee8245fb
garrycnx <garrycnx@gmail.com>
2026-01-25 19:24:20 +0530

    Initial commit - Deep Computer website

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,13 +4,18 @@ import aadhaar from "../../assets/services/aadhaar.png";
 import gnm from "../../assets/services/number_plate.png";
 import passport from "../../assets/services/passport.png";
 import driving from "../../assets/services/Driving.png";
+import rc from "../../assets/services/rc.png";
+import itr from "../../assets/services/itr.png";
+import badc from "../../assets/services/badc.png";
 
 const services = [
   { name: "Aadhaar Card",punjabi: "ਆਧਾਰ ਕਾਰਡ", img: aadhaar },
   { name: "Government Number Plate",punjabi: "ਸਰਕਾਰੀ ਨੰਬਰ ਪਲੇਟ", img: gnm },
   { name: "passport",punjabi: "ਪਾਸਪੋਰਟ", img: passport },
-  { name: "Driving License",punjabi: "ਡਰਾਈਵਿੰਗ ਲਾਇਸੰਸ", img: driving }  
-
+  { name: "Driving License",punjabi: "ਡਰਾਈਵਿੰਗ ਲਾਇਸੰਸ", img: driving },
+  { name: "Car/Bike RC Renew",punjabi: "ਕਾਰ/ਬਾਈਕ ਆਰਸੀ ਰੀਨਿਊ", img: rc },  
+  { name: "Income Tax Return",punjabi: "ਇਨਕਮ ਟੈਕਸ ਰਿਟਰਨ", img: itr },  
+  { name: "Birth/Death certificate",punjabi: "ਜਨਮ/ਮੌਤ ਸਰਟੀਫਿਕੇਟ", img: badc }   
 ];
 
 const Services = () => {
