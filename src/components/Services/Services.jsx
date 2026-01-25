8b4e562e51a9a91ebd51a650b836746c31a5d235
garrycnx <garrycnx@gmail.com>
2026-01-25 19:45:17 +0530

    Services added

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -7,6 +7,10 @@ import driving from "../../assets/services/Driving.png";
 import rc from "../../assets/services/rc.png";
 import itr from "../../assets/services/itr.png";
 import badc from "../../assets/services/badc.png";
+import rsc from "../../assets/services/rsc.png";
+import caste from "../../assets/services/caste.png";
+import lic from "../../assets/services/lic.png";
+import oldage from "../../assets/services/oldage.png";
 
 const services = [
   { name: "Aadhaar Card",punjabi: "ਆਧਾਰ ਕਾਰਡ", img: aadhaar },
@@ -15,7 +19,13 @@ const services = [
   { name: "Driving License",punjabi: "ਡਰਾਈਵਿੰਗ ਲਾਇਸੰਸ", img: driving },
   { name: "Car/Bike RC Renew",punjabi: "ਕਾਰ/ਬਾਈਕ ਆਰਸੀ ਰੀਨਿਊ", img: rc },  
   { name: "Income Tax Return",punjabi: "ਇਨਕਮ ਟੈਕਸ ਰਿਟਰਨ", img: itr },  
-  { name: "Birth/Death certificate",punjabi: "ਜਨਮ/ਮੌਤ ਸਰਟੀਫਿਕੇਟ", img: badc }   
+  { name: "Birth/Death certificate",punjabi: "ਜਨਮ/ਮੌਤ ਸਰਟੀਫਿਕੇਟ", img: badc },   
+  { name: "Resident Certificate",punjabi: "ਨਿਵਾਸੀ ਸਰਟੀਫਿਕੇਟ", img: rsc },  
+  { name: "Caste Certificate",punjabi: "ਜਾਤੀ ਸਰਟੀਫਿਕੇਟ", img: caste }, 
+  { name: "Low Income Certificate",punjabi: "ਘੱਟ ਆਮਦਨੀ ਦਾ ਸਰਟੀਫਿਕੇਟ", img: lic },
+  { name: "Old Age Pension",punjabi: "ਬੁਢਾਪਾ ਪੈਨਸ਼ਨ", img: oldage }
+  
+  
 ];
 
 const Services = () => {
